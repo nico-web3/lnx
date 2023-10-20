22183c47cee7bad13798e19481e73dfeb9b7c8d8
nico-web3 <nicoshishkincartesi@gmail.com>
2023-10-20 14:01:39 +0100

    Update

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -53,7 +53,7 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       // Replace with your project's social card
-      image: 'img/docusaurus-social-card.jpg',
+      image: 'img/lnx-social-card.jpg',
       colorMode: {
         disableSwitch: true,
       },
