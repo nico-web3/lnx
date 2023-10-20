1663ed5f4767ee0ae179705a0e3d93d08cec1896
nico-web3 <nicoshishkincartesi@gmail.com>
2023-10-20 14:33:19 +0100

    Update

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -53,7 +53,7 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       // Replace with your project's social card
-      image: 'img/lnx-social-card.png',
+      image: 'img/social-card.png',
       metaDescription: "Cross-chain Cartesi dApps made easy",
       colorMode: {
         disableSwitch: true,
